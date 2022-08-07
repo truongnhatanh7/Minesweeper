052dc76e450b7df7d671937bca138dc2befb9af8
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-08-07 14:46:56 +0700

    initial commit

---
+++ Minesweeper/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  Minesweeper
+//
+//  Created by Truong Nhat Anh on 07/08/2022.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
